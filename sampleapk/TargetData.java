f4d4bf1c129320a297caff776bcb7be75eb84bb8
NoahZeNairwhal <122690215+NoahZeNairwhal@users.noreply.github.com>
2023-06-11 16:16:37 -0500

    Commented TargetData.java

--- sampleapk/TargetData.java
+++ sampleapk/TargetData.java
@@ -4,9 +4,12 @@ import gov.nasa.arc.astrobee.types.Point;
 import gov.nasa.arc.astrobee.types.Quaternion;
 
 public class TargetData {
+    //The intial position of all the targets, + the qr code, + the goal
     public static moveData[] intial = initialize();
+    //The updated position of all the targets, + the qr code, + the goal (updateTarget changes it)
     public static moveData[] updated = initialize();
 
+    //All the moveData representations of the intial values of the targets and whatnot
     public static moveData[] initialize() {
         return new moveData[]{new moveData(new Point(11.2746, -9.92284, 5.2988), new Quaternion(0f, 0f, -0.707f, 0.707f)),
             new moveData(new Point(10.612, -9.0709, 4.48), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f)),
@@ -18,7 +21,7 @@ public class TargetData {
             new moveData(new Point(11.143, -6.7607, 4.9654), new Quaternion(0f, 0f, -0.707f, 0.707f))};
     }
 
-    //Updates target data using current position as well as a target number (1-8)
+    //Updates target data using current position as well as the target number (1-8)
     public static void updateTarget(int targetNum) {
         updated[targetNum - 1] = new moveData();
     }
