7092015962e5318a14faa604d2359467abbca3c2
NoahZeNairwhal <122690215+NoahZeNairwhal@users.noreply.github.com>
2023-06-15 00:51:41 -0500

    Update TargetData.java

--- sampleapk/TargetData.java
+++ sampleapk/TargetData.java
@@ -17,7 +17,7 @@ public class TargetData {
             new moveData(new Point(10.51, -6.7185, 5.1804), new Quaternion(0f, 0f, -1f, 0f)),
             new moveData(new Point(11.114, -7.9756, 5.3393), new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f)),
             new moveData(new Point(11.355, -8.9929, 4.7818), new Quaternion(0f, 0f, 0f, 1f)),
-            new moveData(new Point(11.381944, -8.566172, 4.5), new Quaternion(0f, 1f, 0f, 0f)), //Actual z of qr is 3.76203, but that's out of bounds
+            new moveData(new Point(11.381944, -8.566172, 4.5), new Quaternion(0f, 0.707f, 0f, -0.707f)), //Actual z of qr is 3.76203, but that's out of bounds
             new moveData(new Point(11.143, -6.7607, 4.9654), new Quaternion(0f, 0f, -0.707f, 0.707f))};
     }
 
