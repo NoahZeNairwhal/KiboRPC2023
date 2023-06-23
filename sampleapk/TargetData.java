d018ff50764cd59a12e2cb989066614d96b03bc8
NoahZeNairwhal <122690215+NoahZeNairwhal@users.noreply.github.com>
2023-06-23 18:58:30 -0500

    Update TargetData.java

--- sampleapk/TargetData.java
+++ sampleapk/TargetData.java
@@ -11,13 +11,13 @@ public class TargetData {
 
     //All the moveData representations of the intial values of the targets and whatnot
     public static moveData[] initialize() {
-        return new moveData[]{new moveData(new Point(11.2746 - 0.067, -9.92284, 5.2988 + 0.174), new Quaternion(0f, 0f, -0.707f, 0.707f)),
+        return new moveData[]{new moveData(new Point(11.2056, -9.92284, 5.4768), new Quaternion(0f, 0f, -0.707f, 0.707f)),
                 /*new moveData(new Point(10.612, -9.0709, 4.48), new Quaternion(0.5f, 0.5f, -0.5f, 0.5f)),*/
                 new moveData(new Point(10.513384 + 0.107, -9.085172 - 0.0572, 4.5), new Quaternion(0f, 0.707f, 0f, 0.707f)),
                 new moveData(new Point(10.71 + 0.003, -7.7 - 0.068, 4.48), new Quaternion(0f, 0.707f, 0f, 0.707f)),
                 new moveData(new Point(10.51, -6.7185 + 0.104, 5.1804 + 0.027), new Quaternion(0f, 0f, -1f, 0f)),
                 new moveData(new Point(11.114 - 0.068, -7.9756 + 0.059, 5.3393), new Quaternion(-0.5f, -0.5f, -0.5f, 0.5f)),
-                new moveData(new Point(11.355, -8.9929 - 0.052, 4.7818 + 0.12), new Quaternion(0f, 0f, 0f, 1f)),
+                new moveData(new Point(11.355, -9.0369, 4.9378), new Quaternion(0f, 0f, 0f, 1f)),
                 new moveData(new Point(11.381944, -8.566172, 4.5), new Quaternion(0f, 0.707f, 0f, -0.707f)), //Actual z of qr is 3.76203, but that's out of bounds
                 new moveData(new Point(11.143, -6.7607, 4.9654), new Quaternion(0f, 0f, -0.707f, 0.707f))};
     }
